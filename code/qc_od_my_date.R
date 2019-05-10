e629f00051d832531b6fc94b40032ddc91feaca1
ivanhanigan <ivan.hanigan@gmail.com>
2019-05-10 10:20:59 +1000

    remove cruft

--- code/qc_od_my_date.R
+++
@@ -1,4 +0,0 @@
-day_i = grep("20160506_0400", my_dates)
-qc2_shp <- r_stdyreg
-qc2_shp@data <- left_join(qc2_shp@data, qc[qc$TIMEPOINT == my_dates[day_i],], by = c("gid" = "AHI_ID"))
-writeOGR(qc2_shp, "working_temporary", "qc2_shp", driver = "ESRI Shapefile")
